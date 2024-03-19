17d545263220eec6c4318de98d64af8406f7f9a9
Pablo Rubiano <pcrubianoa@gmail.com>
2024-03-18 20:10:33 -0500

    Update _layout.tsx

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,18 +1,25 @@
-import { Tabs } from 'expo-router';
+import { useState } from 'react';
+import { Tabs, useSegments } from 'expo-router';
 import { Feather, EvilIcons, SimpleLineIcons, MaterialIcons, AntDesign  } from '@expo/vector-icons';
 
 export default function TabLayout() {
+  
+  const segment = useSegments();
+  console.log(segment[2]);
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: "#1973e7",
+        tabBarInactiveTintColor: "#000",
         tabBarStyle: {
           height: 55,
           // borderWidth: 1,
           // borderRadius: 50,
-          // borderTopEndRadius: 10,
-          // borderTopStartRadius: 10,
-          //backgroundColor: '#1973e7',
+          // borderTopEndRadius: 15,
+          // borderTopStartRadius: 15,
+          // backgroundColor: '#1973e7',
           },
       }}
     >
@@ -21,11 +28,10 @@ export default function TabLayout() {
         options={{
           title: 'Mesas',
           tabBarIcon: ({ size, color }) => (
-            <AntDesign name="team" size={26} color="black" />
+            <AntDesign name={segment[2] === "mesas" ? "appstore1" : "appstore-o"} size={size} color={color} />
           ),
           tabBarLabelStyle: {
             fontSize: 12,
-            color: 'black',
             fontWeight: 'bold'
           },
         }}
@@ -35,11 +41,10 @@ export default function TabLayout() {
         options={{
           title: 'Productos',
           tabBarIcon: ({ size, color }) => (
-            <AntDesign name="tago" size={26} color="black" />
+            <AntDesign name={segment[2] === undefined ? "tags" : "tagso"} size={size} color={color} />
           ),
           tabBarLabelStyle: {
             fontSize: 12,
-            color: 'black',
             fontWeight: 'bold'
           },
         }}
@@ -49,11 +54,10 @@ export default function TabLayout() {
         options={{
           title: 'Mi Perfil',
           tabBarIcon: ({ size, color }) => (
-            <AntDesign name="user" size={26} color="black" />
+            <AntDesign name="user" size={26} color={color} />
           ),
           tabBarLabelStyle: {
             fontSize: 12,
-            color: 'black',
             fontWeight: 'bold'
           },
         }}
@@ -63,11 +67,10 @@ export default function TabLayout() {
         options={{
           title: 'Configuración',
           tabBarIcon: ({ size, color }) => (
-            <AntDesign name="setting" size={26} color="black" />
+            <AntDesign name="setting" size={26} color={color} />
           ),
           tabBarLabelStyle: {
             fontSize: 12,
-            color: 'black',
             fontWeight: 'bold'
           },
         }}
